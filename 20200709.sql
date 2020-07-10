db92cf03a46b698b48469ddc8be52f5f72d03673
kjy93 <kjy930912@gmail.com>
2020-07-10 12:44:23 +0900

    "dml update delete
    트랜잭션
    isolation level
     lv0 : read uncommtied
     lv1 : read commited
     lv2 : repeatable read
     lv3 : serializable read
    
    DDL
    객체 삭제(DROP)
    객체 생성(CREATE)
    
    제약조건
     컬럼 레벨 PK 제약조건"

--- 20200709.sql
+++ 20200709.sql
@@ -51,7 +51,7 @@ SELECT *
 FROM emp
 WHERE EXISTS (SELECT 'X'
               FROM dual);
-              
+
 일반적으로 EXISTS 연산자는 상호연관 서브쿼리에서 실행된다.
 1. 사원정보를 조회 하는데
 2. WHERE m.empno = e.mgr 조건을 만족하는 사원만 조회
@@ -66,7 +66,7 @@ WHERE EXISTS (SELECT 'X'
 ==> 서브쿼리가 [확인자]로 사용되었다.
     비상호연관의 경우 서브쿼리가 먼저 실행될 수도 있다.
     ==> 서브쿼리가 [제공자]로 사용되었다.
-              
+
 --실습 sub8
 매니저가 존재하는 사원 정보 조회, 단 서브쿼리를 사용하지 않고
 SELECT e.*
@@ -94,7 +94,10 @@ WHERE EXISTS (SELECT 'X'
               FROM cycle
               WHERE cid = 1
                 AND cycle.pid = product.pid);                
-            
+
+SELECT *
+FROM product;
+
 --실습 sub10
 SELECT *
 FROM product
@@ -102,7 +105,7 @@ WHERE NOT EXISTS (SELECT 'X'
                   FROM cycle
                   WHERE cid = 1
                   AND cycle.pid = product.pid);
-                  
+
 집합연산
 sql에서 데이터를 확장하는 방법
 가로 확장(컬럼을 확장) : JOIN 
@@ -117,7 +120,7 @@ SQL에서 제공하는 집합 연산자
 합집합 : UNION / UNION ALL
         두개의 집합을 하나로 합칠 때, 두 집합에 속하는 요소는 한 번만 표현된다
         {1, 2, 3} U {1, 4, 5} ==> {1, 1, 2, 3, 4, 5} ==> {1, 2, 3, 4, 5}
-        
+
 UNION과 UNION ALL의 차이
 UNION : 수학의 집합 연산과 동일
         위의 집합과 아래 집합에서 중복되는 데이터를 한 번 제거
@@ -125,13 +128,13 @@ UNION : 수학의 집합 연산과 동일
 UNION ALL : 합집합의 정의와 다르게 중복을 허용
             위의 집합과 아래 집합의 행을 붙이는 행위만 실시
             중복을 찾는 과정이 없기 때문에 속도면에서 빠르다
-            
+
 개발자가 두 집합의 중복이 없다는 것을 알고 있으면 UNION 보다는 UNION ALL을 사용 하는게 좋다.            
-        
+
 교집합 : INTERSECT 
         두개의 집합에서 서로 중복되는 요소만 별도의 집합으로 생성
         {1, 2, 3} 교집합 {1, 4, 5} ==> {1, 1} ==> {1}
-        
+
 차집합 : MINUS
         앞에 선언된 집합의 요소중 뒤에 선언된 집합의 요소를 제거하고 남은 요소로 새로운 집합을 생성
         {1, 2, 3} 차집합 {1, 4, 5} ==> {2, 3}
@@ -143,7 +146,7 @@ UNION ALL : 합집합의 정의와 다르게 중복을 허용
         {1, 2, 3} 차집합 {1, 4, 5} ==> {2, 3}
         {1, 4, 5} 차집합 {1, 3, 4} ==> {4, 5}
 
-        
+
 UNION 연산자
 집합연산을 하려는 두개의 집합이 동일하기 때문에 합집합을 하면 중복을 허용하지 않기 때문에 
 7566, 7698 사번을 갖는 사원이 한번씩만 조회가 된다
@@ -221,7 +224,7 @@ DML-INSERT : 테이블에 데이터를 입력하는 SQL 문장;
 문법
 INSERT INTO 테이블 (컬럼1, 컬럼2...) 
             VALUES (컬럼1값, 컬럼2값...);
-            
+
 dept 테이블에 99번 부서번호를 갖는 ddit를 부서명으로 daejeon 지역에 위치하는 부서를 등록
 
 INSERT INTO dept (deptno, dname, loc)
@@ -231,7 +234,7 @@ INSERT INTO dept (deptno, dname, loc)
 다만 VALUES 절에 기술한 해당 컬럼에 입력할 값의 위치만 지키면 된다
 INSERT INTO dept (deptno, dname, loc)
           VALUES (99, 'ddit', 'daejeon');
-          
+
 만약 테이블의 모든 컬럼에 대해 값을 입력하고자 할 경우는 컬럼을 나열하지 않아도 된다.
 단, VALUES 절에 입력할 값을 기술할 순서는 테이블에 정의된 컬럼의 순서와 동일 해야한다.
 
@@ -257,7 +260,7 @@ INSERT INTO emp (hiredate, job, empno)
          VALUES (SYSDATE, 'RANGER', 9998);
 INSERT INTO emp (hiredate, job, empno, ename)
          VALUES (TO_DATE('2020/07/01', 'YYYY/MM/DD'), 'RANGER', 9997, 'moon');         
-         
+
 
 위에서 실행한 INSERT 구문들이 모두 취소
 ROLLBACK;
@@ -276,7 +279,6 @@ FROM dual
 UNION ALL
 SELECT TO_DATE('2020/07/01', 'YYYY/MM/DD'), 'RANGER', 9997, 'moon'
 FROM dual;
-
 
 
 UPDATE : 테이블에 존재하는 데이터를 수정하는 것
@@ -310,10 +312,10 @@ FROM dept
 WHERE deptno = 90;
 
 
-UPDATE dept SET dname = '대덕it',
+UPDATE dept SET dname = '대전it',
                 loc = 'daejeon'
 WHERE deptno = 90;
-                
+
 
 SELECT *
 FROM dept;
